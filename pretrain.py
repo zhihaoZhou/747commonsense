2db1bf129d156560d401f47f66664585ef4fc1cf
Zhihao <zhouzhihao90@gmail.com>
2019-04-23 16:43:45 -0400

    Update pretrain.py

--- pretrain.py
+++ pretrain.py
@@ -12,15 +12,24 @@ def spacy_tok(x):
     return [tok.text for tok in my_tok.tokenizer(x)]
 
 
-if __name__ == '__main__':
+class Config:
+    batch_size = 5
+    bptt_len = 10
     file_path = 'pretrain_data/raw_stories.txt'
+
+
+
+if __name__ == '__main__':
+    config = Config()
     TEXT = data.Field(lower=True, tokenize=spacy_tok)
-    train = datasets.LanguageModelingDataset(file_path, TEXT, newline_eos=False)
+    train = datasets.LanguageModelingDataset(config.file_path, TEXT, newline_eos=False)
     print(len(train))
 
     TEXT.build_vocab(train)
-    train_iter = data.BPTTIterator(train, batch_size=5, bptt_len=30)
+    train_iter = data.BPTTIterator(train, batch_size=config.batch_size, bptt_len=config.bptt_len)
 
     for batch in train_iter:
-        print(batch)
+        print(batch.text)
+        print('~'*80)
+        print(batch.target)
         break

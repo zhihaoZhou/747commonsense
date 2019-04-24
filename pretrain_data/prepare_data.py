8e766dcbc678dcedc4887fc44745a039e324a524
Zhihao <zhouzhihao90@gmail.com>
2019-04-23 21:44:02 -0400

    data

--- pretrain_data/prepare_data.py
+++ pretrain_data/prepare_data.py
@@ -1,21 +1,17 @@
-import os
-
-directory = '.'
+f_list = ['mcscript.raw.txt']
 
 # join all raw txt to one str
 train_raw_str = ''
 dev_raw_str = ''
-for file in os.listdir(directory):
-    filename = os.fsdecode(file)
-    if filename.endswith(".raw.txt"):
-        with open(os.path.join(directory, filename)) as f_tmp:
-            for line in f_tmp:
-                train_raw_str += line[:int(len(line) * 0.8)]
-                dev_raw_str += line[int(len(line) * 0.8):]
+for f_name in f_list:
+    with open(f_name) as f_tmp:
+        for line in f_tmp:
+            train_raw_str += line[:int(len(line) * 0.9)]
+            dev_raw_str += line[int(len(line) * 0.9):]
 print(len(train_raw_str))
 print(len(dev_raw_str))
 
-# write 80% of the str to train, 20% to dev
+# write 90% of the str to train, 10% to dev
 with open('lm.train', 'w') as f:
     f.write(train_raw_str)
 with open('lm.dev', 'w') as f:

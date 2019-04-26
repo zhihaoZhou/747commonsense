841390e4330473d568b16c3b30276f677f309b28
Zhihao <zhouzhihao90@gmail.com>
2019-04-25 20:33:28 -0400

    Update prepare_data.py

--- pretrain_data/prepare_data.py
+++ pretrain_data/prepare_data.py
@@ -16,14 +16,19 @@ for f_name in f_list:
 lm_tok = spacy.load('en')
 lm_tok.tokenizer.add_special_case('<unk>', [{ORTH: '<unk>'}])
 
+
+def spacy_tok(x):
+    return [tok.text for tok in lm_tok.tokenizer(x)]
+
+
 # write 90% of the str to train, 10% to dev
 with open('lm.train', 'w') as f:
     # tokenize then write
-    train_raw_str = ' '.join(lm_tok.tokenizer(train_raw_str))
+    train_raw_str = ' '.join(spacy_tok(train_raw_str))
     print(len(train_raw_str))
     f.write(train_raw_str)
 with open('lm.dev', 'w') as f:
     # tokenize then write
-    dev_raw_str = ' '.join(lm_tok.tokenizer(dev_raw_str))
+    dev_raw_str = ' '.join(spacy_tok(dev_raw_str))
     print(len(dev_raw_str))
     f.write(dev_raw_str)
